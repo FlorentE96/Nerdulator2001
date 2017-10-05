419937e819c3733c613cb4d49d0d0a164526e064
FlorentE96 <flo.ecochard@gmail.com>
2017-10-04 22:19:50 -0300

    First stable version

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 #include <stdint.h>
 #include <iostream>
 
+#define IS_DIGIT_BUTTON(x) (x>=50000)
+#define IS_OP_BUTTON(x) (x<50000 && x>=40000)
+
 //EnableWindow()  //disable buttons when in BIN mode
 
 HINSTANCE hInst;
 int16_t disp_num;
 int16_t result;
-bool flag;
+bool flag, flagMake = true;
+
 int16_t operand1 = 0;
 int16_t operand2 = 0;
-int16_t * operand_ptr = &operand1;
 
 typedef enum OpCodes {NOOP = 0, DIV, MUL, SUM, SUB, AND, OR, XOR, NOT} OpCodes;
 OpCodes currentOp;
@@ -29,44 +32,46 @@ void setResultValue(int inc)
     {
         disp_num *= 10;
         disp_num += inc;
-        *operand_ptr = disp_num;
     }
 }
 
 void makeCalculation() {
-    if(operand_ptr == &operand1)
-        operand_ptr = &operand2;
-    else
-        operand_ptr = &operand1;
-
     switch(currentOp) {
     case SUM:
-        std::cout << operand1 << "+" << operand2 << std::endl;
         result = operand1 + operand2;
+        std::cout << operand1 << "+" << operand2 << "=" << result << std::endl;
         break;
     case SUB:
-        std::cout << operand1 << "-" << operand2 << std::endl;
         result = operand1 - operand2;
+        std::cout << operand1 << "-" << operand2 << "=" << result << std::endl;
         break;
     case DIV:
-        result = disp_num / result;
+        result = operand1 / operand2;
+        std::cout << operand1 << "/" << operand2 << "=" << result << std::endl;
         break;
     case MUL:
-        result = disp_num * result;
+        result = operand1 * operand2;
+        std::cout << operand1 << "*" << operand2 << "=" << result << std::endl;
         break;
     case NOT:
         result = ~disp_num;
         break;
     case AND:
-        result = disp_num & result;
+        result = operand1 & operand2;
         break;
     case OR:
-        result = disp_num | result;
+        result = operand1 | operand2;
         break;
     case XOR:
-        result = disp_num ^ result;
+        result = operand1 ^ operand2;
+        break;
+    default:
+        operand1 = disp_num;
+        flag = true;
+        return;
         break;
     }
+    operand1 = result;
     disp_num = result;
     flag = true;
 }
@@ -81,6 +86,7 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
         result = 0;
         CheckRadioButton(hwndDlg, SEL_DEC, SEL_BIN, SEL_DEC);
         SetDlgItemInt(hwndDlg, RESULT_BOX,0,0);
+        std::cout << "::: Nerdulator2001 History :::" << std::endl;
         break;
     }
     return TRUE;
@@ -95,88 +101,100 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         if(HIWORD(wParam) == BN_CLICKED)
         {
-            switch(LOWORD(wParam))
+            if(LOWORD(wParam)>=50000)
             {
-            case BUT_QUIT:
-                if(MessageBox(hwndDlg, "Do you really want to quit?", "Confirmation", MB_OKCANCEL) != IDCANCEL)
-                    EndDialog(hwndDlg,0);
-                break;
-            case BUT_ABOUT:
-                MessageBox(hwndDlg, "blablabla", "About", MB_OK);
-                break;
-            case DIGIT1:
-                setResultValue(1);
-                break;
-            case DIGIT2:
-                setResultValue(2);
-                break;
-            case DIGIT3:
-                setResultValue(3);
-                break;
-            case DIGIT4:
-                setResultValue(4);
-                break;
-            case DIGIT5:
-                setResultValue(5);
-                break;
-            case DIGIT6:
-                setResultValue(6);
-                break;
-            case DIGIT7:
-                setResultValue(7);
-                break;
-            case DIGIT8:
-                setResultValue(8);
-                break;
-            case DIGIT9:
-                setResultValue(9);
-                break;
-            case DIGIT0:
-                setResultValue(0);
-                break;
-            case OP_SUM:
-                currentOp = SUM;
+                switch(LOWORD(wParam))
+                {
+                case DIGIT1:
+                    setResultValue(1);
+                    break;
+                case DIGIT2:
+                    setResultValue(2);
+                    break;
+                case DIGIT3:
+                    setResultValue(3);
+                    break;
+                case DIGIT4:
+                    setResultValue(4);
+                    break;
+                case DIGIT5:
+                    setResultValue(5);
+                    break;
+                case DIGIT6:
+                    setResultValue(6);
+                    break;
+                case DIGIT7:
+                    setResultValue(7);
+                    break;
+                case DIGIT8:
+                    setResultValue(8);
+                    break;
+                case DIGIT9:
+                    setResultValue(9);
+                    break;
+                case DIGIT0:
+                    setResultValue(0);
+                    break;
+                }
+                flagMake = true;
+            } else if(IS_OP_BUTTON(LOWORD(wParam)))
+            {
+                if(flagMake) {
+                    operand2 = disp_num;
+                    makeCalculation();
+                }
+                switch(LOWORD(wParam))
+                {
+                case BUT_QUIT:
+                    if(MessageBox(hwndDlg, "Do you really want to quit?", "Confirmation", MB_OKCANCEL) != IDCANCEL)
+                        EndDialog(hwndDlg,0);
+                    break;
+                case BUT_ABOUT:
+                    MessageBox(hwndDlg, "We are the coolest.", "About", MB_OK);
+                    break;
+                case OP_SUM:
+                    currentOp = SUM;
+                    break;
+                case OP_SUB:
+                    currentOp = SUB;
+                    break;
+                case OP_DIV:
+                    currentOp = DIV;
+                    break;
+                case OP_MUL:
+                    currentOp = MUL;
+                    break;
+                case OP_AND:
+                    currentOp = AND;
+                    break;
+                case OP_OR:
+                    currentOp = OR;
+                    break;
+                case OP_NOT:
+                    currentOp = NOT;
+                    break;
+                case OP_XOR:
+                    currentOp = XOR;
+                    break;
+                case BUT_C:
+                    currentOp = NOOP;
+                    disp_num = 0;
+                    operand1 = 0;
+                    operand2 = 0;
+                    result=0;
+                    break;
+                }
+                flagMake = false;
+            } else
+            {
                 makeCalculation();
-                break;
-            case OP_SUB:
-                currentOp = SUB;
-                makeCalculation();
-                break;
-            case OP_DIV:
-                currentOp = DIV;
-                makeCalculation();
-                break;
-            case OP_MUL:
-                currentOp = MUL;
-                makeCalculation();
-                break;
-            case OP_AND:
-                currentOp = AND;
-                makeCalculation();
-                break;
-            case OP_OR:
-                currentOp = OR;
-                makeCalculation();
-                break;
-            case OP_NOT:
-                currentOp = NOT;
-                makeCalculation();
-                break;
-            case OP_XOR:
-                currentOp = XOR;
-                makeCalculation();
-                break;
-            case OP_EQ:
-                makeCalculation();
-                break;
-            case BUT_C:
-                currentOp = NOOP;
-                disp_num = 0;
-                result=0;
-                break;
+                flagMake = false;
             }
-            SetDlgItemInt(hwndDlg, RESULT_BOX,disp_num,0);
+            char myString[1024];
+            sprintf(myString, "%d", disp_num);
+            SetDlgItemText(hwndDlg, RESULT_BOX,myString);
         }
+
     }
     return TRUE;
     }
--- resource.h
+++ resource.h
@@ -4,16 +4,16 @@
 
 #define IDD_DIALOG1                             100
 #define RESULT_BOX                              40000
-#define DIGIT1                                  40001
-#define DIGIT2                                  40002
-#define DIGIT3                                  40003
-#define DIGIT5                                  40004
-#define DIGIT4                                  40005
-#define DIGIT6                                  40006
-#define DIGIT7                                  40007
-#define DIGIT8                                  40008
-#define DIGIT9                                  40009
-#define DIGIT0                                  40010
+#define DIGIT1                                  50001
+#define DIGIT2                                  50002
+#define DIGIT3                                  50003
+#define DIGIT5                                  50004
+#define DIGIT4                                  50005
+#define DIGIT6                                  50006
+#define DIGIT7                                  50007
+#define DIGIT8                                  50008
+#define DIGIT9                                  50009
+#define DIGIT0                                  50010
 #define OP_DIV                                  40011
 #define OP_MUL                                  40012
 #define OP_SUM                                  40013
@@ -26,5 +26,5 @@
 #define SEL_BIN                                 40020
 #define BUT_QUIT                                40021
 #define BUT_ABOUT                               40022
-#define OP_EQ                                   40023
+#define OP_EQ                                   30023
 #define BUT_C                                   40024
